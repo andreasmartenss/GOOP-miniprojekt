a5181a492102dd775595024ba34f8f7855b0fccc
andreasmartenss <amarte24@student.aau.dk>
2026-01-18 12:05:10 +0100

    Update GUI.java

--- src/main/java/org/example/GUI/GUI.java
+++ src/main/java/org/example/GUI/GUI.java
@@ -1,8 +1,19 @@
 package org.example.GUI;
 
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class GUI extends Scene {
 
-    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStageRef = primaryStage;
+
+        primaryStage.setScene(ScannerPage.createScene());
+
+        primaryStage.setTitle("Productlist");
+        primaryStage.show();
+
+    }
+
 }
