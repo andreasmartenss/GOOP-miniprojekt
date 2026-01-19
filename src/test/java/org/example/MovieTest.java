7ac1d93944a8f15666a8759f4b530a3617508b63
andreasmartenss <amarte24@student.aau.dk>
2026-01-19 11:40:11 +0100

    test

---
+++ src/test/java/org/example/MovieTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRating() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGenre() {
+    }
+}
